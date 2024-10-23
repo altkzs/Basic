55a51e8b88ecac9a4392e3da1a8edbf63260d1d5
altkzs <dogmagoa@gmail.com>
2024-10-23 13:00:02 +0900

    fix:이동 구현중

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+	private int animationVariable = 0;
+	private Animator animator;
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+	void onMove()
+	{
+
+	}
+	void onJump()
+	{
+
+	}
+}
